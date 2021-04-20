a0b86a15b82bbfaed442dac018d57b7b6bab4c55
Bidhan <Bidhanbaniya789@gmail.com>
2021-04-20 16:16:50 +0545

    this is master branch that is using helper ip add

--- app/Http/Controllers/QrTrafficController.php
+++ app/Http/Controllers/QrTrafficController.php
@@ -5,11 +5,19 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\qr_traffic;
 use App\Models\qr_codr;
+use App\Helpers\UserSystemInfoHelper;
+
 class QrTrafficController extends Controller
 {
     public function index($id)
     {
-        $qr = qr_codr::findOrFail($id);
+        $qr = qr_codr::findOrFail($id)->where('status',1);
+
+        // $getip = UserSystemInfoHelper::get_ip(); ->this scripts doesnt work on localhost so using third party api
+       
+        $getos = UserSystemInfoHelper::get_os();
+        $getdevice = UserSystemInfoHelper::get_device(); 
+        $getbrowser = UserSystemInfoHelper::get_browsers();
         
     }    
 }
